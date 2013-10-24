03f58ab18e8f0e5d0721c3eeb9a98667964c480b
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-10-24 14:12:40 +0200

    Added seeds for many to many between tags and snippets

--- Snippets/Migrations/Configuration.cs
+++ Snippets/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace Snippets.Migrations
     using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Diagnostics;
     using System.Linq;
     using Snippets.Models;
 
@@ -20,58 +21,75 @@ namespace Snippets.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
-            //
 
-                context.Snippets.AddOrUpdate(s => s.Title,
-                    new Snippet
-                    {
-                        Title = "This is my first snippet",
-                        Body = "This is the body of my snippet, which doesn't look anything like a snippet",
-                        Categories = new List<Category>(), 
-                        Tags = new List<Tag>(),
-                        VisibilityId = context.Visibilities.Single( s => s.Label == "Public").ID
-                    },
-                    new Snippet
-                    {
-                        Title = "This is my second snippet",
-                        Body = "Just some weird stuff in here to function as a second snippet and yeah, this snippet is protected",
-                        Categories = new List<Category>(), 
-                        Tags = new List<Tag>(),
-                        VisibilityId = context.Visibilities.Single( s => s.Label == "Protected").ID
-                    }
-                );
+            context.Snippets.AddOrUpdate(s => s.Title,
+                new Snippet
+                {
+                    Title = "This is my first snippet",
+                    Body = "This is the body of my snippet, which doesn't look anything like a snippet",
+                    Categories = new List<Category>(), 
+                    Tags = new List<Tag>(),
+                    VisibilityId = context.Visibilities.Single( s => s.Label == "Public").ID
+                },
+                new Snippet
+                {
+                    Title = "This is my second snippet",
+                    Body = "Just some weird stuff in here to function as a second snippet and yeah, this snippet is protected",
+                    Categories = new List<Category>(), 
+                    Tags = new List<Tag>(),
+                    VisibilityId = context.Visibilities.Single( s => s.Label == "Protected").ID
+                }
+            );
 
-                context.Visibilities.AddOrUpdate(v => v.Label,
-                  new Visibility { 
-                      Label = "Hidden",
-                      Description = "Only visible for administrators"
-                  },
-                  new Visibility
-                  {
-                      Label = "Protected",
-                      Description = "Only visible to logged in uses"
-                  },
-                  new Visibility
-                  {
-                      Label = "Public",
-                      Description = "Visible to everyone"
-                  }
-                );
+            context.Visibilities.AddOrUpdate(v => v.Label,
+                new Visibility { 
+                    Label = "Hidden",
+                    Description = "Only visible for administrators"
+                },
+                new Visibility
+                {
+                    Label = "Protected",
+                    Description = "Only visible to logged in uses"
+                },
+                new Visibility
+                {
+                    Label = "Public",
+                    Description = "Visible to everyone"
+                }
+            );
 
-                context.Tags.AddOrUpdate(t => t.Label,
-                    new Tag
-                    {
-                        Label = "PHP"
-                    },
-                    new Tag
-                    {
-                        Label = "JavaScript"
-                    },
-                    new Tag
-                    {
-                        Label = "Tricks"
-                    }
-                );
+            context.Tags.AddOrUpdate(t => t.Label,
+                new Tag
+                {
+                    Label = "PHP"
+                },
+                new Tag
+                {
+                    Label = "JavaScript"
+                },
+                new Tag
+                {
+                    Label = "Tricks"
+                }
+            );
+
+            AddOrUpdateTag(context, 1, "PHP");
+            AddOrUpdateTag(context, 2, "Tricks");
+
+            context.SaveChanges();
+
         }
+
+        // Add tags to the snippets
+        private void AddOrUpdateTag(SnippetsDBContext context, int snippetId, string tagLabel) {
+            Snippet snippet = context.Snippets.SingleOrDefault(s => s.ID == snippetId);
+
+            Tag tag = snippet.Tags.SingleOrDefault(t => t.Label == tagLabel);
+
+            if(tag == null) {
+                snippet.Tags.Add(context.Tags.Single(t => t.Label == tagLabel));
+            }
+        }
+
     }
 }
