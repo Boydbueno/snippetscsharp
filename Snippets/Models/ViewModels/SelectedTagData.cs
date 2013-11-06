69d01311bc7bc27806907e15e397d4e57e5264ff
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-11-06 01:10:33 +0100

    Added a model for holding data for checking if tag is selected on snippet

---
+++ Snippets/Models/ViewModels/SelectedTagData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Snippets.Models.ViewModels
+{
+    public class SelectedTagData
+    {
+
+        public int TagId { get; set; }
+        public string Label { get; set; }
+        public bool Selected { get; set; }
+
+    }
+}
