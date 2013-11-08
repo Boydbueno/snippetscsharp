b75bb88df55af2d32013d1934c11225af08d876f
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-11-08 13:32:21 +0100

    Added creation of snippets from homepage when logged as admin and fixed snippets visibility

---
+++ Snippets/Controllers/SnippetsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Snippets.Models;
+
+namespace Snippets.Controllers
+{
+    public class SnippetsController : Controller
+    {
+
+        private SnippetsDBContext db = new SnippetsDBContext();
+
+        //
+        // GET: /Snippets/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.VisibilityId = new SelectList(db.Visibilities, "ID", "Label");
+            return View();
+        }
+
+        //
+        // POST: /Snippets/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Snippet snippet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Snippets.Add(snippet);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.VisibilityId = new SelectList(db.Visibilities, "ID", "Label", snippet.VisibilityId);
+            return View(snippet);
+        }
+
+    }
+}
