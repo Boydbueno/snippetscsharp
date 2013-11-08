09a81b372eae9368474b7809d6ae57086ecd8456
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-11-08 15:57:46 +0100

    Created an 'extras' page to show some stuff

---
+++ Snippets/Controllers/ExtrasController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Snippets.Areas.Admin.Models.ViewModels;
+using Snippets.Models;
+using Snippets.Models.ViewModels;
+
+namespace Snippets.Controllers
+{
+    public class ExtrasController : Controller
+    {
+        private SnippetsDBContext db = new SnippetsDBContext();
+
+        //
+        // GET: /Extras/
+
+        public ActionResult Index()
+        {
+            ExtraVM extraVM = new ExtraVM();
+
+            List<SelectedTagData> selectedTagData = new List<SelectedTagData>();
+
+            // For the sake of the 'demo' we'll just deselect all checkboxes
+            foreach (Tag tag in db.Tags.ToList())
+            {
+                selectedTagData.Add(new SelectedTagData
+                {
+                    TagId = tag.ID,
+                    Label = tag.Label,
+                    Selected = false
+                });
+            }
+
+            extraVM.Tags = selectedTagData;
+            extraVM.Visibilies = db.Visibilities.ToList();
+
+            return View(extraVM);
+        }
+
+    }
+}
---
+++ Snippets/Models/ViewModels/ExtraVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Snippets.Areas.Admin.Models.ViewModels;
+
+namespace Snippets.Models.ViewModels
+{
+    public class ExtraVM
+    {
+        public List<SelectedTagData> Tags { get; set; }
+        public List<Visibility> Visibilies { get; set; }
+    }
+}
